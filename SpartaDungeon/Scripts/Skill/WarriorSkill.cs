cb30532ab98ef7de6d290216c2a17d7865ff5bc0
pm2979 <pm29798512@gmail.com>
2025-04-25 10:46:34 +0900

    스킬 폴더 추가

--- SpartaDungeon/Scripts/Skill/WarriorSkill.cs
+++ SpartaDungeon/Scripts/Skill/WarriorSkill.cs
@@ -13,7 +13,7 @@ namespace SpartaDungeon
     {
         public float Damage { get; private set; }
 
-        public PawerStrike() : base("파워 스트라이크", "MP 12 소비, 적 하나에 데미지 260%", 12) 
+        public PawerStrike() : base("파워 스트라이크", "MP 12 소비, 적 하나에 데미지 260%", 12)
         {
             Damage = 2.6f;
         }
@@ -32,7 +32,7 @@ namespace SpartaDungeon
     {
         public float Damage { get; private set; }
 
-        public SlashBlust() : base("슬래시 블러스트", "MP 30 소비, 모든 적에게 데미지 130%", 30) 
+        public SlashBlust() : base("슬래시 블러스트", "MP 30 소비, 모든 적에게 데미지 130%", 30)
         {
             Damage = 1.3f;
         }
@@ -41,10 +41,10 @@ namespace SpartaDungeon
         {
             Console.WriteLine($"\n\nLv.{attacker.Level} [{attacker.Name}] 의 [{Name}]!\n");
 
-            foreach( Monster monster in monsters )
+            foreach (Monster monster in monsters)
             {
-                if(monster.IsDead ==  false )
-                attacker.DamageResult(monster, Damage);
+                if (monster.IsDead == false)
+                    attacker.DamageResult(monster, Damage);
             }
 
             attacker.UseMP(MP);
